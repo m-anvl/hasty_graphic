d4436ab814688d127f79a70a7b45837eb249986d
AnatoliiM18 <mamaev-av18@ya.ru>
2025-01-22 00:27:56 +0400

    Define base colors

--- src/hasty_graphic.h
+++ src/hasty_graphic.h
@@ -9,6 +9,18 @@
 #define ERR_SDL(str) do { fprintf(stderr, "[%s:%d] %s error: %s\n",\
                         __FILE__, __LINE__, #str, SDL_GetError()); } while(0)
 
+/* Colors defines */
+#define BLACK   0xff000000u
+#define WHITE   0xffffffffu
+#define RED     0xff0000ffu
+#define GREEN   0xff00ff00u
+#define BLUE    0xffff0000u
+#define CYAN    0xffffff00u
+#define MAGENTA 0xffff00ffu
+#define YELLOW  0xff00ffffu
+#define GREY    0xff808080u
+#define ORANGE  0xff0080ffu
+
 #ifdef __cplusplus
 extern "C" {
 #endif
